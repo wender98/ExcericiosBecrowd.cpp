819da5e7e682b3d9a042ca1cecec3fa07bb2f87e
Wender <wenderrferreira89@gmail.com>
2025-04-24 16:30:26 -0300

    Create EX1011.cpp
    Faça um programa que calcule e mostre o volume de uma esfera sendo fornecido o valor de seu raio (R). A fórmula para calcular o volume é: (4/3) * pi * R3. Considere (atribua) para pi o valor 3.14159.
    
    Dica: Ao utilizar a fórmula, procure usar (4/3.0) ou (4.0/3), pois algumas linguagens (dentre elas o C++), assumem que o resultado da divisão entre dois inteiros é outro inteiro.
    
    Entrada
    O arquivo de entrada contém um valor de ponto flutuante (dupla precisão), correspondente ao raio da esfera.
    
    Saída
    A saída deverá ser uma mensagem "VOLUME" conforme o exemplo fornecido abaixo, com um espaço antes e um espaço depois da igualdade. O valor deverá ser apresentado com 3 casas após o ponto.
    
    LINK:  https://judge.beecrowd.com/pt/problems/view/1011

---
+++ EX1011.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    double raio,volume, pi = 3.14159;
+
+    scanf("%lf", &raio);
+
+    volume = (4.0/3) * pi * (raio * raio * raio);
+
+    printf("VOLUME = %.3lf", volume);
+
+    return 0;
+
+}
